f4a4a9fa4ed7ed9e07c41135c07db82d1e24205d
Pavel Kosarev <pavel999kosarev@gmail.com>
2023-08-24 17:27:02 +0300

    add

--- framework/templator.py
+++ framework/templator.py
@@ -1,11 +1,12 @@
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
 from os.path import join
 
 
 def render(template_name, folder='templates', **kwargs):
     file_path = join(folder, template_name)
-
-    with open(file_path, encoding='utf-8') as f:
-        template = Template(f.read())
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
 
     return template.render(**kwargs)
+
